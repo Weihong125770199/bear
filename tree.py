81577220e00460f804fc7f741388b4b782a26d3b
chenweihong <chenweihong@ubuntu.(none)>
2018-02-11 16:50:07 -1000

    add test file

---
+++ tree.py
@@ -0,0 +1,8 @@
+import Shannon as Sn
+dataSet = [[1,1,'yes'],
+           [1,1,'yes'],
+           [1,0,'no' ],
+           [0,1,'no' ],
+           [0,1,'no' ]]
+shnnonEnt = Sn.calcShannonEnt(dataSet)
+print shnnonEnt
